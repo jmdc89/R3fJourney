52975c0d70d7fb1f8fb5e7216a61f432985ab5ef
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 15:02:44 +0300

    Experience
    init

---
+++ src/Experience.jsx
@@ -0,0 +1,12 @@
+
+export default function Experience() {
+    return (
+        <>
+            <mesh>
+                <torusKnotGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
+        
+    )
+}
