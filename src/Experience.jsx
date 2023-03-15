9f5ec3bdc62b58cdd989178111aba8292486a5c5
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 15:13:39 +0300

    Update Experience.jsx
    geometry test

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,9 +2,19 @@
 export default function Experience() {
     return (
         <>
-            <mesh>
-                <torusKnotGeometry />
-                <meshNormalMaterial />
+            <mesh position-x="-2">
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" wireframe />
+            </mesh>
+
+            <mesh rotation-y={ Math.PI * 0.25 } position-x="2" scale={2}>
+                <boxGeometry />
+                <meshBasicMaterial color="mediumpurple" wireframe={false} />
+            </mesh>
+
+            <mesh rotation-x={ - Math.PI * 0.5 } position-y="-1" scale={10}>
+                <planeBufferGeometry/>
+                <meshBasicMaterial color="green"/>
             </mesh>
         </>
         
