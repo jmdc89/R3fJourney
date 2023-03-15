d6040db79b5621c3f120b5cd3f184e6610b2c6e8
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 16:02:15 +0300

    Update CustomObject.jsx
    useMemo optimization

--- src/CustomObject.jsx
+++ src/CustomObject.jsx
@@ -1,11 +1,21 @@
 import * as THREE from 'three'
+import { useMemo } from 'react'
 
 
 export default function CustomObject() {
 
     const verticesCount = 10 * 3
-    const positions = new Float32Array(verticesCount * 3)
 
+    const positions = useMemo(() =>
+    {
+        const positions = new Float32Array(verticesCount * 3)
+        
+        for(let i = 0; i < verticesCount * 3; i++)
+            positions[i] = (Math.random() - 0.5) * 3
+
+        return positions
+    })
+    
     for (let i = 0; i < verticesCount * 3; i++)
         positions[i] = (Math.random() - 0.5) * 3
 
