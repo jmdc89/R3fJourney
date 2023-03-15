c1ff43a4b103dd303666006ee756cb07bde33802
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 16:14:53 +0300

    Update Experience.jsx
    rotating camera

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -17,14 +17,19 @@ export default function Experience() {
     useFrame((state, delta) => {
         cubeRef.current.rotation.y += delta
         // groupRef.current.rotation.y += delta
+
+        const angle = state.clock.elapsedTime
+        state.camera.position.x = Math.sin(angle) * 8
+        state.camera.position.z = Math.cos(angle) * 8
+        state.camera.lookAt(0, 0, 0)
     })
 
     return (
         <>
-            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-            <ambientLight intensity={ 0.5 } />
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            <ambientLight intensity={0.5} />
 
-            <orbitControls args={[camera, gl.domElement]} />
+            {/* <orbitControls args={[camera, gl.domElement]} /> */}
 
             <group ref={groupRef}>
                 <mesh position-x="-2">
