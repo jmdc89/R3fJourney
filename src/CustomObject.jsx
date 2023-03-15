45db3ea0a44a7aeb4da5ddc5c4df9e5f9a84f748
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 16:05:54 +0300

    Update CustomObject.jsx
    useEffect

--- src/CustomObject.jsx
+++ src/CustomObject.jsx
@@ -1,33 +1,35 @@
 import * as THREE from 'three'
-import { useMemo } from 'react'
+import { useEffect, useRef, useMemo } from 'react'
 
 
 export default function CustomObject() {
 
+    const geometryRef = useRef()
+
     const verticesCount = 10 * 3
 
-    const positions = useMemo(() =>
-    {
+    const positions = useMemo(() => {
         const positions = new Float32Array(verticesCount * 3)
-        
-        for(let i = 0; i < verticesCount * 3; i++)
+
+        for (let i = 0; i < verticesCount * 3; i++)
             positions[i] = (Math.random() - 0.5) * 3
 
         return positions
     })
-    
-    for (let i = 0; i < verticesCount * 3; i++)
-        positions[i] = (Math.random() - 0.5) * 3
+
+    useEffect(() => {
+        geometryRef.current.computeVertexNormals()
+    }, [positions])
 
     return <mesh>
-        <bufferGeometry>
-        <bufferAttribute
-        attach="attributes-position"
-        count={ verticesCount }
-        itemSize={ 3 }
-        array={ positions }
-    />
+        <bufferGeometry ref={geometryRef}>
+            <bufferAttribute
+                attach="attributes-position"
+                count={verticesCount}
+                itemSize={3}
+                array={positions}
+            />
         </bufferGeometry>
-        <meshBasicMaterial color="red" side={ THREE.DoubleSide } />
+        <meshStandardMaterial color="red" side={THREE.DoubleSide} />
     </mesh>
 }
