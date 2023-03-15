e389793927f5617ee7d89a135f10814f74322b56
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-15 15:22:45 +0300

    Update Experience.jsx
    group rotation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,29 +6,32 @@ import { useRef } from 'react'
 export default function Experience() {
 
     const cubeRef = useRef()
+    const groupRef = useRef()
 
-    useFrame((state, delta) =>
-    {
+    useFrame((state, delta) => {
         cubeRef.current.rotation.y += delta
+        // groupRef.current.rotation.y += delta
     })
 
     return (
         <>
-            <mesh position-x="-2">
-                <sphereGeometry />
-                <meshBasicMaterial color="orange" wireframe />
-            </mesh>
+            <group ref={groupRef}>
+                <mesh position-x="-2">
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange" wireframe />
+                </mesh>
 
-            <mesh ref={cubeRef} rotation-y={ Math.PI * 0.25 } position-x="2" scale={2}>
-                <boxGeometry />
-                <meshBasicMaterial color="mediumpurple" wireframe={false} />
-            </mesh>
+                <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x="2" scale={2}>
+                    <boxGeometry />
+                    <meshBasicMaterial color="mediumpurple" wireframe={false} />
+                </mesh>
+            </group>
 
-            <mesh rotation-x={ - Math.PI * 0.5 } position-y="-1" scale={10}>
-                <planeBufferGeometry/>
-                <meshBasicMaterial color="green"/>
+            <mesh rotation-x={- Math.PI * 0.5} position-y="-1" scale={10}>
+                <planeBufferGeometry />
+                <meshBasicMaterial color="green" />
             </mesh>
         </>
-        
+
     )
 }
