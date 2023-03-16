d0315329df8468069c20e3e432a098b46bc6d1c3
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-16 16:24:52 +0300

    Update Experience.jsx
    debugging init

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -16,48 +16,20 @@ export default function Experience() {
         <mesh position-x={- 2} ref={sphere}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-            {/* <Html position={ [ 1, 1, 0 ] } wrapperClass="label" center distanceFactor={ 8 } occlude={ [ sphere, cube ] }>That's a sphere 👍</Html> */}
         </mesh>
 
-        <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={4}
-            axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
-            scale={2}>
+
             <mesh ref={cube} position-x={2} scale={1.5}>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
-        </PivotControls>
-
-        {/* <TransformControls object={ cube } mode="translate" /> */}
 
 
         <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
-            <MeshReflectorMaterial
-                resolution={512}
-                blur={[1000, 1000]}
-                mixBlur={1}
-                // mirror={ 0.5 }
-                color="greenyellow"
-            />
+            <meshStandardMaterial color="green" />
 
         </mesh>
-
-        {/* <Html>Test</Html> */}
-
-        <Float
-            speed={5}
-            floatIntensity={2}
-        >
-            <Text
-                fontSize={1}
-                color="salmon"
-                position-y={2}
-                maxWidth={2}
-                textAlign="center"
-            >I LOVE R3F
-            </Text>
-        </Float>
 
     </>
 }
