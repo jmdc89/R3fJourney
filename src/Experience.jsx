6eaa5c6d66fd9553f8d4fa4d2eef3c8f2d01f7fc
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-16 15:22:53 +0300

    Update Experience.jsx
    PivotControls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,33 +1,35 @@
 import { useThree, extend, useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
-import { TransformControls, OrbitControls } from '@react-three/drei'
+import { PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
 
 
 
-export default function Experience()
-{
+export default function Experience() {
     const cube = useRef()
 
     return <>
-        <OrbitControls makeDefault/>
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <OrbitControls makeDefault />
+        <directionalLight position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-       
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <PivotControls anchor={[0, 0, 0]} depthTest={false} lineWidth={4}
+            axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
+            scale={2}>
+            <mesh ref={cube} position-x={2} scale={1.5}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+        </PivotControls>
 
-        <TransformControls object={ cube } />
-        
+        {/* <TransformControls object={ cube } mode="translate" /> */}
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
