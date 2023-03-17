5d306641df7332434e72d4c09e8ab613781a2726
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-17 15:31:44 +0300

    Update Experience.jsx
    leva range

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,28 +1,40 @@
 import { useThree, extend, useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 import { MeshReflectorMaterial, Float, Text, Html, PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
-
+import { useControls } from 'leva'
 
 
 export default function Experience() {
     const cube = useRef()
     const sphere = useRef()
 
+    const { position } = useControls({
+        position:
+        {
+            value: - 2,
+            min: - 4,
+            max: 4,
+            step: 0.01
+        }
+
+    })
+
+
     return <>
         <OrbitControls makeDefault />
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <mesh position-x={- 2} ref={sphere}>
+        <mesh position-x={position} ref={sphere}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
 
-            <mesh ref={cube} position-x={2} scale={1.5}>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+        <mesh ref={cube} position-x={2} scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
 
 
         <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
