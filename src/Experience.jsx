5106e5808c62eae919e87f25465cde1c895d3ffb
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-19 11:46:21 +0300

    Update Experience.jsx
    hamburguer load

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default function Experience()
 {
+    const model = useLoader(GLTFLoader, './hamburger.glb')
+    console.log(model)
+     
     return <>
 
         <Perf position="top-left" />
@@ -11,6 +16,8 @@ export default function Experience()
 
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
+
+        <primitive object={ model.scene } scale={ 0.35 } />
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
