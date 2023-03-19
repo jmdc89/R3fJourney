b83bf79a51cddef5d1bcd4b8765e61921e05155f
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-19 12:13:36 +0300

    Lazy load
    suspense component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,19 +1,9 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import Model from './Model.jsx'
+import { Suspense } from 'react'
 
 export default function Experience() {
-    const model = useLoader(
-        GLTFLoader,
-        './hamburger-draco.glb',
-        (loader) => {
-            const dracoLoader = new DRACOLoader()
-            dracoLoader.setDecoderPath('./draco/')
-            loader.setDRACOLoader(dracoLoader)
-        }
-    )
 
     return <>
 
@@ -24,7 +14,9 @@ export default function Experience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <primitive object={model.scene} scale={0.35} />
+        <Suspense>
+            <Model />
+        </Suspense>
 
         <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
