6bfb78a739e0ee73a21cc6ee3315ec93d6ab1774
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-19 11:48:56 +0300

    Update Experience.jsx
    dracoLoader

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,24 +2,31 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
-export default function Experience()
-{
-    const model = useLoader(GLTFLoader, './hamburger.glb')
-    console.log(model)
-     
+export default function Experience() {
+    const model = useLoader(
+        GLTFLoader,
+        './hamburger-draco.glb',
+        (loader) => {
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('./draco/')
+            loader.setDRACOLoader(dracoLoader)
+        }
+    )
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <primitive object={ model.scene } scale={ 0.35 } />
+        <primitive object={model.scene} scale={0.35} />
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
