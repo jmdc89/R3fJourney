63dc3f90fdfa4315c13dd689732ccbb0d52b3215
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-19 11:41:09 +0300

    Update Experience.jsx
    mesh delete

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -12,16 +12,6 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
