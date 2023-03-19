2eb449a9911da363456c58655716485e21c4ecb1
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-19 11:37:28 +0300

    Update index.jsx
    loader pack

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,20 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience'
-import { CineonToneMapping } from 'three'
-import * as THREE from 'three'
-import { StrictMode } from 'react'
-
+import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-
 root.render(
-    <StrictMode>
-        <Canvas 
-            shadows
-            gl={ {
-                antialias: true,
-                toneMapping: THREE.ACESFilmicToneMapping
-            } }
-            camera={ { 
-            fov: 45, 
-            near: 0.1, 
-            far: 200, 
-            position: [ 3, 2, 6 ] 
-            } }
-        >
-            <color args={ [ 'ivory' ] } attach="background" />
-            <Experience />
-        </Canvas>
-        </StrictMode>
+    <Canvas
+        shadows
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
 )
