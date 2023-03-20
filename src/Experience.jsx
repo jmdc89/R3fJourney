6b262a2e9185780af1fb49d7b5ab271614270d16
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-20 15:36:12 +0300

    Update Experience.jsx
    attributes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,7 @@
 import { Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
 
         <Perf position="top-left" />
@@ -10,10 +9,20 @@ export default function Experience()
         <OrbitControls makeDefault />
 
         <Center>
-        <Text3D font="./fonts/helvetiker_regular.typeface.json">
-            HELLO R3F
-            <meshNormalMaterial />
-        </Text3D>
+            <Text3D
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={0.75}
+                height={0.2}
+                curveSegments={12}
+                bevelEnabled
+                bevelThickness={0.02}
+                bevelSize={0.02}
+                bevelOffset={0}
+                bevelSegments={5}
+            >
+                HELLO R3F
+                <meshNormalMaterial />
+            </Text3D>
         </Center>
 
     </>
