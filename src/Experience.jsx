8c5e7ef54b5e629f6f7eadc4b111662835e7650d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-20 14:39:28 +0300

    Experience setup 3d text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,34 +1,17 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import Model from './Model.jsx'
-import { Suspense } from 'react'
-import Placeholder from './Placeholder.jsx'
-import Hamburguer from './Hamburguer.jsx'
-import Fox from './Fox.jsx'
-import Unicorn from './Unicorn.jsx'
-import Boxes from './boxestest.jsx'
 
-export default function Experience() {
-
+export default function Experience()
+{
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={ 0.04 } />
-        <ambientLight intensity={0.5} />
-
-        <Suspense fallback={ <Placeholder position-y={ 0.5 } scale={ [ 2, 3, 2 ] } /> }>
-            <Hamburguer scale={0.35}/>
-            <Fox/>
-            {/* <Unicorn /> */}
-            <Boxes />
-        </Suspense>
-
-        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+        <mesh scale={ 1.5 }>
+            <boxGeometry />
+            <meshNormalMaterial />
         </mesh>
 
     </>
