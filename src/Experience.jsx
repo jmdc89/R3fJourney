c97c10a6bc0c8ac4aacfc556ca0a871769fae54c
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-20 15:34:15 +0300

    Update Experience.jsx
    Center

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Text3D, OrbitControls } from '@react-three/drei'
+import { Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
 export default function Experience()
@@ -9,10 +9,12 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
+        <Center>
         <Text3D font="./fonts/helvetiker_regular.typeface.json">
             HELLO R3F
             <meshNormalMaterial />
         </Text3D>
+        </Center>
 
     </>
 }
