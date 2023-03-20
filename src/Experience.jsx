752e455384d550b2beb7d59c0276b21764214997
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-20 16:01:33 +0300

    Update Experience.jsx
    random torus

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,10 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
-export default function Experience() 
-{
+export default function Experience() {
 
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
     console.log(matcapTexture)
-
 
     return <>
 
@@ -27,14 +25,29 @@ export default function Experience()
                 bevelSegments={5}
             >
                 HELLO R3F
-                <meshMatcapMaterial matcap={ matcapTexture } />
+                <meshMatcapMaterial matcap={matcapTexture} />
             </Text3D>
         </Center>
 
-        <mesh>
-            <torusGeometry args={ [ 1, 0.6, 16, 32 ] } />
-            <meshMatcapMaterial matcap={ matcapTexture } />
-        </mesh>
+        {[...Array(100)].map((value,index) =>
+            <mesh
+                key={index}
+                position={[
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ]}
+                scale={ 0.2 + Math.random() * 0.2 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
+            >
+                <torusGeometry args={[1, 0.6, 16, 32]} />
+                <meshMatcapMaterial matcap={matcapTexture} />
+            </mesh>
+        )}
 
     </>
 }
