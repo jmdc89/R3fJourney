27e743b6a3bb445e69de62c5f0b705f6e394c546
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-21 15:26:47 +0300

    Update Experience.jsx
    poleLights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { useTexture, useGLTF, OrbitControls } from '@react-three/drei'
+import { Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience() {
 
@@ -13,9 +13,20 @@ export default function Experience() {
 
         <OrbitControls makeDefault />
 
-        <mesh geometry={nodes.baked.geometry}>
-            <meshBasicMaterial map={bakedTexture} />
-        </mesh>
+        <Center>
+
+            <mesh geometry={nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture} />
+            </mesh>
+
+            <mesh geometry={nodes.poleLightA.geometry} position={nodes.poleLightA.position}>
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            <mesh geometry={nodes.poleLightB.geometry} position={nodes.poleLightB.position}>
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+        </Center>
 
     </>
 }
