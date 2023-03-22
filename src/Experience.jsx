1a72430884cc5abc32471d2dcd6d216bbfb95008
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-22 09:12:31 +0300

    Update Experience.jsx
    model load

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,18 @@
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
+
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
+
     return <>
 
+        <color args={ [ '#241a1a' ] } attach="background" />
+
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+
+        <primitive object={ computer.scene } />
 
     </>
 }
