e4a045b1c686cb94687af9bd3fa0ee1c1c43fee1
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-22 09:33:14 +0300

    Update Experience.jsx
    Contactshadows

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { PresentationControls, Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { ContactShadows, PresentationControls, Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -29,6 +29,13 @@ export default function Experience()
         </Float>
 
         </PresentationControls>
+
+        <ContactShadows 
+        position-y={ - 1.4 }
+        opacity={ 0.4 }
+        scale={ 5 }
+        blur={ 2.4 }
+        />
 
     </>
 }
